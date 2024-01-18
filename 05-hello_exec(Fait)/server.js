c0d7ab05a37af653c5f9d53db2b154bb3d3c19eb
Linus <117490184+Kioma349@users.noreply.github.com>
2024-01-18 17:37:48 +0100

    Commit recente 2.5 Exo 6  Debut

---
+++ 05-hello_exec(Fait)/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+// Configuration de la connexion à la base de données
+const pool = new Pool({
+  user: 'linus',
+  host: 'localhost', // Utilisez 'mon-conteneur-postgres' si vous utilisez Docker Network
+  database: 'linus_db',
+  password: 'linus1001',
+  port: 5432,
+});
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Route pour récupérer les données de la table 'personnes'
+app.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM personnes');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Démarrage du serveur
+app.listen(PORT, () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
